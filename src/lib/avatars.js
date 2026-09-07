a7e9e04086b7fece2cef36f564c01523dfb97a02
Antonio Ruiz <contacto.antonioruiz@gmail.com>
2026-09-07 23:49:31 +0200

    Optimize avatar icon bundle

--- src/lib/avatars.js
+++ src/lib/avatars.js
@@ -1,22 +1,28 @@
-import * as GiIcons from "react-icons/gi";
+import {
+  GiLion, GiWizardStaff, GiWolfHead, GiWyvern, GiZebraShield, GiYinYang,
+  GiXylophone, GiZeppelin, GiWitchFace, GiWizardFace, GiWoodenHelmet, GiWorld,
+  GiWolverineClaws, GiHummingbird, GiHorseHead, GiHoodedAssassin, GiHoneycomb,
+  GiHydra, GiIceGolem, GiImperialCrown, GiIncomingRocket, GiInvisibleFace,
+  GiJellyfish, GiLightningTrio,
+} from "react-icons/gi";
 
 export const AVATAR_COLORS = [
   "#5B8DEF", "#7C5CE7", "#E85D75", "#F28C4B", "#E5B83B", "#56B870",
   "#2FB7A8", "#2F9BD8", "#9B59B6", "#D96CBE", "#8B6F47", "#5F6B7A",
 ];
 
-export const AVATAR_ICON_NAMES = [
-  "GiLion", "GiFox", "GiPanda", "GiRabbit", "GiOwl", "GiTurtle", "GiOctopus", "GiSharkFin",
-  "GiDragonHead", "GiDinosaurBones", "GiUnicorn", "GiRobotGolem", "GiAstronautHelmet", "GiRocket",
-  "GiCrownedSkull", "GiCrown", "GiShield", "GiSoccerBall", "GiBasketballBall", "GiTennisRacket",
-  "GiLightningTrio", "GiStarSwirl", "GiFire", "GiCrystalBall", "GiPirateFlag", "GiNinjaHeroicStance",
-  "GiWizardStaff", "GiKnightBanner", "GiPlanetCore", "GiCometSpark",
-].filter((name) => typeof GiIcons[name] === "function");
+export const AVATARS = [
+  ["GiLion", GiLion], ["GiWizardStaff", GiWizardStaff], ["GiWolfHead", GiWolfHead], ["GiWyvern", GiWyvern],
+  ["GiZebraShield", GiZebraShield], ["GiYinYang", GiYinYang], ["GiXylophone", GiXylophone], ["GiZeppelin", GiZeppelin],
+  ["GiWitchFace", GiWitchFace], ["GiWizardFace", GiWizardFace], ["GiWoodenHelmet", GiWoodenHelmet], ["GiWorld", GiWorld],
+  ["GiWolverineClaws", GiWolverineClaws], ["GiHummingbird", GiHummingbird], ["GiHorseHead", GiHorseHead], ["GiHoodedAssassin", GiHoodedAssassin],
+  ["GiHoneycomb", GiHoneycomb], ["GiHydra", GiHydra], ["GiIceGolem", GiIceGolem], ["GiImperialCrown", GiImperialCrown],
+  ["GiIncomingRocket", GiIncomingRocket], ["GiInvisibleFace", GiInvisibleFace], ["GiJellyfish", GiJellyfish], ["GiLightningTrio", GiLightningTrio],
+];
 
-export const DEFAULT_AVATAR_ICON = AVATAR_ICON_NAMES[0] || "GiAbstract050";
+export const AVATAR_ICON_NAMES = AVATARS.map(([name]) => name);
+export const DEFAULT_AVATAR_ICON = "GiLion";
 export const DEFAULT_AVATAR_COLOR = "#5B8DEF";
-
-export const AVATARS = AVATAR_ICON_NAMES.map((name) => [name, GiIcons[name]]);
 
 export function normalizeAvatar(icon, color) {
   return {
@@ -26,5 +32,5 @@ export function normalizeAvatar(icon, color) {
 }
 
 export function avatarComponent(name) {
-  return GiIcons[name] || GiIcons[DEFAULT_AVATAR_ICON] || GiIcons.GiAbstract050;
+  return AVATARS.find(([iconName]) => iconName === name)?.[1] || GiLion;
 }
