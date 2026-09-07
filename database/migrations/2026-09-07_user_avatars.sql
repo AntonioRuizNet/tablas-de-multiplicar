37d247843ac2d0b298b1def991ebdb19686bf97b
Antonio Ruiz <contacto.antonioruiz@gmail.com>
2026-09-07 23:47:54 +0200

    Set safe avatar defaults

--- database/migrations/2026-09-07_user_avatars.sql
+++ database/migrations/2026-09-07_user_avatars.sql
@@ -6,3 +6,9 @@ UPDATE users
 SET avatar_icon = COALESCE(avatar_icon, 'GiLion'),
     avatar_color = COALESCE(avatar_color, '#5B8DEF')
 WHERE avatar_icon IS NULL OR avatar_color IS NULL;
+
+ALTER TABLE users
+  ALTER COLUMN avatar_icon SET DEFAULT 'GiLion',
+  ALTER COLUMN avatar_icon SET NOT NULL,
+  ALTER COLUMN avatar_color SET DEFAULT '#5B8DEF',
+  ALTER COLUMN avatar_color SET NOT NULL;
